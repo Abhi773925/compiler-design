f21a0e694b28ae6b859d4af150b6a37dbfb1ff00
Abhi773925 <rockabhisheksingh778189@gmail.com>
2025-10-07 23:47:39 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HeroSection from './components/landingpage/HeroSection'
+
+function App() {
+
+  return (
+    <div className='h-full w-full'>
+     <HeroSection/>
+      
+    </div>
+  )
+}
+
+export default App

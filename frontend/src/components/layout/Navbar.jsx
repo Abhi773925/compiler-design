4f0097c3a558126fc44e89dd51f588725534ce76
Abhi773925 <rockabhisheksingh778189@gmail.com>
2025-10-10 00:49:19 +0530

    issue fixed

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
               transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
               <Link to="/" className="flex items-center">
-                <img src={prepmate} className="h-[60px] w-[50px]"></img>
+                <img src={prepmate} className="h-[40px] w-[30px]"></img>
               </Link>
             </motion.div>
 
